0ac7cf639e59ffead2dbe647c275d6a7e9884547
Ilan <smilan273@gmail.com>
2022-12-17 23:14:37 +0200

    init project

---
+++ Arrays.h
@@ -0,0 +1,6 @@
+#ifndef C_EX3_ARRAYS_H
+#define C_EX3_ARRAYS_H
+
+void hello(void);
+
+#endif //C_EX3_ARRAYS_H
---
+++ Strings.h
@@ -0,0 +1,8 @@
+//
+// Created by 97253 on 17/12/2022.
+//
+
+#ifndef C_EX3_STRINGS_H
+#define C_EX3_STRINGS_H
+
+#endif //C_EX3_STRINGS_H
